b7b02a1f130f5bd46f016569260b97807f84617a
Hafid.Haddouti <code@haddouti.com>
2021-09-19 16:50:36 +0200

    Update script to install k8s-external-secrets

--- scripts/ibm-cloud-sm-update-secret.sh
+++ scripts/ibm-cloud-sm-update-secret.sh
@@ -5,6 +5,6 @@
 SECRET_ID=$1
 
 currentDate=`date '+%Y-%m-%d_%H:%M:%S'`
-ibmcloud secrets-manager secret-update --secret-type username_password --action rotate --id $SECRET_ID --body '{"password": "mega-important-${currentDate}"}'
+ibmcloud secrets-manager secret-update --secret-type username_password --action rotate --id $SECRET_ID --body '{"password": "mega-important-'${currentDate}'"}'
 
 
