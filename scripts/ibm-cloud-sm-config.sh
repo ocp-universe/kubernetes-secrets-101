882924cc6b07ab0d0d4069856defa6b5b626ca34
Hafid.Haddouti <code@haddouti.com>
2021-09-19 15:27:50 +0200

    Update script to install k8s-external-secrets

--- scripts/ibm-cloud-sm-config.sh
+++ scripts/ibm-cloud-sm-config.sh
@@ -6,22 +6,27 @@
 
 # create Service ID and API Key
 
-export SERVICE_ID=`ibmcloud iam service-id kubernetes-secrets-demo --output json | jq -r ".id"`
+echo "ServiceID..."
+export SERVICE_ID=`ibmcloud iam service-id kubernetes-secrets-demo --output json | jq -r ".[].id"`
 if [ -z "${SERVICE_ID}"]; then
     export SERVICE_ID=`ibmcloud iam service-id-create kubernetes-secrets-demo --description "A service ID for testing Secrets Manager and Kubernetes Service." --output json | jq -r ".id"`; echo "ServiceID: $SERVICE_ID"
     ibmcloud iam service-policy-create $SERVICE_ID --roles "SecretsReader" --service-name secrets-manager
 fi
 
+echo "API Key..."
 export IBM_CLOUD_API_KEY=`ibmcloud iam service-api-key kubernetes-secrets-demo $SERVICE_ID --output json | jq -r ".apikey"`
 if [ -z "${IBM_CLOUD_API_KEY}"]; then
     export IBM_CLOUD_API_KEY=`ibmcloud iam service-api-key-create kubernetes-secrets-demo $SERVICE_ID --description "An API key for testing Secrets Manager." --output json | jq -r ".apikey"`
 fi
 
 # Prepare Secrets Manager with secret group and dummy secret
+echo "SecretsManagerUrl..."
 export SECRETS_MANAGER_URL=`ibmcloud resource service-instance secrets-manager --output json | jq -r '.[].dashboard_url | .[0:-3]'`; echo "SecretsManagerUrl: $SECRETS_MANAGER_URL"
 
+echo "SecretGroup..."
 export SECRET_GROUP_ID=`ibmcloud secrets-manager secret-group-create --resources '[{"name":"sg-demo","description":"Demo App and Secrets."}]' --output json | jq -r ".resources[].id"`; echo "SecretGroupId: $SECRET_GROUP_ID"
 
+echo "Secret..."
 export SECRET_ID=`ibmcloud secrets-manager secret-create --secret-type username_password  --resources '[{"name":"example_username_password","description":"Extended description for my secret.","secret_group_id":"'"$SECRET_GROUP_ID"'","username":"user123","password":"cloudy-rainy-coffee-book","labels":["env-demo","demo"]}]' --output json | jq -r ".resources[].id"`; echo "SecretId: $SECRET_ID"
 
 # Create Secret with API Key, URL and type
