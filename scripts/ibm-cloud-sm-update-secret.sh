fb3a11c1b6c83aefa4f56446a72a0ec164c3574c
Hafid.Haddouti <code@haddouti.com>
2021-12-05 12:31:49 +0100

    Optimize IBMM Secrets Manager and k8s-external-secrets handling/docu

--- scripts/ibm-cloud-sm-update-secret.sh
+++ scripts/ibm-cloud-sm-update-secret.sh
@@ -5,6 +5,9 @@
 SECRET_ID=$1
 
 currentDate=`date '+%Y-%m-%d_%H:%M:%S'`
+
+export SECRETS_MANAGER_URL=`ibmcloud resource service-instance secrets-manager --output json | jq -r '.[].dashboard_url | .[0:-3]'`; echo $SECRETS_MANAGER_URL
+
 ibmcloud secrets-manager secret-update --secret-type username_password --action rotate --id $SECRET_ID --body '{"password": "mega-important-'${currentDate}'"}'
 
 
